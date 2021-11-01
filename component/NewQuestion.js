a66b7b980a08eb2b82e7050638806f0de68291cb
Elcid-Chawa <elcidchawa@gmail.com>
2021-11-01 21:20:00 +0100

    added views code

---
+++ component/NewQuestion.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {View, Text} from 'react-native';
+
+class NewQuestion extends Component {
+    render(){
+        return (
+            <View>
+                <Text>Add New Question</Text>
+            </View>
+        )
+    }
+}
+
+export default NewQuestion;
+
