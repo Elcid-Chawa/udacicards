98d751d8da59fb740f3b3c9d7619c0185d3e5247
Elcid-Chawa <elcidchawa@gmail.com>
2021-11-01 21:04:12 +0100

    modified utils

--- utils/api.js
+++ utils/api.js
@@ -1,30 +1,11 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-data = {
-    React: {
-      title: 'React',
-      questions: [
-        {
-          question: 'What is React?',
-          answer: 'A library for managing user interfaces'
-        },
-        {
-          question: 'Where do you make Ajax requests in React?',
-          answer: 'The componentDidMount lifecycle event'
-        }
-      ]
-    },
-    JavaScript: {
-      title: 'JavaScript',
-      questions: [
-        {
-          question: 'What is a closure?',
-          answer: 'The combination of a function and the lexical environment within which that function was declared.'
-        }
-      ]
-    }
-  }
+export function initDeck() {
+    return decks;
+}
 
-  function initData () {
+export function getDecks(){}
 
-  }
+export function getDeck(){}
+export function saveDeckTitle(){}
+export function addCardToDeck(){}
