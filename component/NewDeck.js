8e23b50763e2c0d5d43e74cf1b6081a0c4e6e17a
Elcid-Chawa <elcidchawa@gmail.com>
2021-11-04 23:14:24 +0100

    added text

--- component/NewDeck.js
+++ component/NewDeck.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
 class NewDeck extends Component {
+    state = {
+            title: '',
+            questions: []
+        }
     render(){
         return (
             <View>
-                <Text>Add New Deck Title</Text>
-                <Text>Create Deck Button</Text>
+                <Text>What is the Title of your nex deck</Text>
+                <Text>Text box</Text>
+                <Button title="Submit" />
             </View>
         )
     }
