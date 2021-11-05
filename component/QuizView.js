753188778a80930517303fbd46e6f21b7ee8132e
Elcid-Chawa <elcidchawa@gmail.com>
2021-11-05 23:15:16 +0100

    fix quiz view

--- component/QuizView.js
+++ component/QuizView.js
@@ -1,23 +1,47 @@
 import React, {Component} from 'react'
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { connect } from 'react-redux';
 
 class QuizView extends Component {
     render(){
+        const { questions } = this.props;
+        console.log(questions)
         return (
-            <View>
-                <Text>Quiz View</Text>
-                <Text>Quiz Question</Text>
-                <Text>Show Answer Button</Text>
-                <Text>Correct Button</Text>
-                <Text>Incorrect Button</Text>
-                <Text>Remaining Questions</Text>
-                <Text>Score</Text>
-                <Text>Restart Quiz</Text>
-                <Text>Back button</Text>
-            </View>
+            <ScrollView>
+                {questions && (
+                    questions.map((q, index )=> (
+                       <View key={index}>
+                        <Text>Quiz View</Text>
+                        <Text>{q.question}</Text>
+                        <Text>Show Answer Button</Text>
+                        <Text style={{display: 'none'}}>{q.answer}</Text>
+                        <Button title="Correct" />
+                        <Button title="Incorrect" />
+                        <Text>Remaining Questions</Text>
+                        <Text>Score</Text>
+                        <Text>Restart Quiz</Text>
+                        <Text>Back button</Text>
+                    </View> 
+                    ))
+                )}
+                
+            </ScrollView>
+                
         )
     }
 }
 
-export default QuizView;
+function mapStateToProps(decks, {navigation, route}){
+    const deckKey = route.params.deckKey
+    const questions = decks[deckKey].questions
+
+    return {
+
+        decks,
+        questions,
+    }
+}
+
+export default connect(mapStateToProps)(QuizView);
 
